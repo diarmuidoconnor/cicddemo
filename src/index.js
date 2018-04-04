076742a9a7e17395acb4cacb37a4a96c849c8ce4
Diarmuid O Connor <doconnor@wit.ie>
2018-04-04 10:58:55 +0100

    Fix CORS issue

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import postsRouter from './api/posts';
 
 import favicon from 'serve-favicon';
 import logger  from 'morgan';
-
+ 
 dotenv.config();
 
 const app = express();
@@ -19,7 +19,28 @@ app.use(bodyParser.urlencoded());
 // app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger(''));
-    app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'));
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use(express.static(__dirname + '/public'));
 
 app.use('/api/contacts', contactsRouter);
 app.use('/api/posts', postsRouter);
